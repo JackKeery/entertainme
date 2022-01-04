76e7e17cee99a943fb0dfaa89f9448c8c9712f62
Jack <jk579@kent.ac.uk>
2022-01-04 16:37:12 +0000

    Initial commit

---
+++ src/main/java/com/jackkeery/entertainme/EntertainmeApplication.java
@@ -0,0 +1,13 @@
+package com.jackkeery.entertainme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntertainmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntertainmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackkeery/entertainme/EntertainmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackkeery.entertainme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntertainmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
